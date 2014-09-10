faee31842d0f35807cfc0ea04376c058f0701d87
MOBI <uta.mobi@gmail.com>
2014-09-10 17:25:55 -0500

    Initial Commit

---
+++ UTARadio/UTARadio/M3UHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace UTARadio
+{
+    class M3UHandler
+    {
+        string BASE_URL;
+        WebClient client;
+
+        public M3UHandler(string baseURL)
+        {
+            if (baseURL == null || baseURL.Length == 0)
+            {
+                throw new ApplicationException("Specify the URI of the resource to retrieve.");
+            }
+            BASE_URL = baseURL;
+
+            //download the first list
+            DownloadChunckListData();
+        }
+
+
+        private void DownloadChunckListData()
+        {
+            client = new WebClient();
+            client.OpenReadCompleted += new OpenReadCompletedEventHandler(client_OpenReadCompleted);
+            client.OpenReadAsync(new Uri(BASE_URL));
+        }
+
+        void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+                byte[] buffer = new byte[1024];
+                e.Result.Read(buffer, 0, buffer.Length);
+
+                string s = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+        }
+
+    }
+}
---
+++ UTARadio/UTARadio/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using UTARadio.Resources;
+
+namespace UTARadio
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+    }
+}
